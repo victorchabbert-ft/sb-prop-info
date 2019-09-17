f70454b702ff197e023c111aca164c27b0b17e02
Victor Chabbert <50482864+victorchabbert-ft@users.noreply.github.com>
2019-09-17 15:02:13 +0200

    Add storybook

---
+++ src/Test.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+export interface TestProps {
+  /**
+   * React children
+   */
+  children?: React.ReactChildren
+}
+
+const Test: React.FC<TestProps> = ({ children }) => (
+  <div>
+    <h1>Testing the info block</h1>
+    {children}
+  </div>
+)
+
+export default Test
---
+++ src/test.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { storiesOf } from '@storybook/react'
+
+import Test from "./Test"
+
+storiesOf("Test", module).add("hello", () => <Test />)
